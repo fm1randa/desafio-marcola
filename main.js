652b29131dcf2b2a9ac473f3082b591144be79cd
Filipe <filipe.rj149@gmail.com>
2020-07-08 19:50:32 -0300

    js updated

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 axios.get("https://reqres.in/api/users")
     .then(function(response){
-        console.log(response.data.data[0].id);
+        //console.log(response.data.data[0].id);
+        tamanho = Object.keys(response.data.data).length;
+        tbody = document.querySelector("table tbody");
+        for (var i = 0; i < tamanho; i++){
+            tr = document.createElement("tr");
+            tbody.appendChild(tr);
+            console.log("tr adicionado");
+        }
+
     })
     .catch(function(error){
         console.warn(error);
