aeb5107e09a8f4fa658ad5052133750f75d11dc4
Filipe <filipe.rj149@gmail.com>
2020-07-08 18:19:35 -0300

    js updated

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 axios.get("https://reqres.in/api/users")
     .then(function(response){
-        console.log(response);
+        console.log(response.data.data[0].id);
     })
     .catch(function(error){
         console.warn(error);
     });
+
+    
